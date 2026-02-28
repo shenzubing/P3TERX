f578f207edb79250df411423177401dde5f301e6
shenzubing <68570155+shenzubing@users.noreply.github.com>
2026-02-28 10:40:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,36 +1,15 @@
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 自动寻找小米 AX1800 的 DTS 文件路径
+DTS_PATH=$(find target/linux/ipq60xx/ -name "*ipq6018-xiaomi-ax1800.dts*" | head -n 1)
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-#!/bin/bash
-# 修改 IP (可选，默认 192.168.1.1)
-# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-
-# --- 核心操作：释放 Reserved 内存 ---
-# 定位小米 AX1800 的设备树文件路径 (注意：不同源码路径可能略有差异)
-DTS_PATH="target/linux/ipq60xx/files/arch/arm64/boot/dts/qcom/ipq6018-xiaomi-ax1800.dts"
-
-# 1. 禁用 Wi-Fi 固件预留内存 (释放约 50MB)
-sed -i '/wlan_fw_mem: wlan_fw_mem@4ab00000/a \ \ \ \ \ \ \ \ status = "disabled";' $DTS_PATH
-
-# 2. 禁用 NSS 核心预留内存 (释放约 30MB)
-sed -i '/nss_core0_mem: nss_core0_mem@40000000/a \ \ \ \ \ \ \ \ status = "disabled";' $DTS_PATH
-sed -i '/nss_core1_mem: nss_core1_mem@40800000/a \ \ \ \ \ \ \ \ status = "disabled";' $DTS_PATH
-
-# 3. 禁用调试日志预留 (释放约 3MB)
-sed -i '/m3_dump: m3_dump@50c00000/a \ \ \ \ \ \ \ \ status = "disabled";' $DTS_PATH
-sed -i '/q6_etr_dump: q6_etr_dump@50b00000/a \ \ \ \ \ \ \ \ status = "disabled";' $DTS_PATH
+if [ -n "$DTS_PATH" ]; then
+    echo "定位到 DTS 文件: $DTS_PATH"
+    # 执行内存手术
+    sed -i '/wlan_fw_mem: wlan_fw_mem@4ab00000/a \ \ \ \ \ \ \ \ status = "disabled";' "$DTS_PATH"
+    sed -i '/nss_core0_mem: nss_core0_mem@40000000/a \ \ \ \ \ \ \ \ status = "disabled";' "$DTS_PATH"
+    sed -i '/nss_core1_mem: nss_core1_mem@40800000/a \ \ \ \ \ \ \ \ status = "disabled";' "$DTS_PATH"
+    sed -i '/m3_dump: m3_dump@50c00000/a \ \ \ \ \ \ \ \ status = "disabled";' "$DTS_PATH"
+    sed -i '/q6_etr_dump: q6_etr_dump@50b00000/a \ \ \ \ \ \ \ \ status = "disabled";' "$DTS_PATH"
+else
+    echo "错误: 找不到 AX1800 的 DTS 文件，请检查源码是否支持该机型"
+    exit 1
+fi
